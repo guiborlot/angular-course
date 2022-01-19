b61daf45436d65e71db607e26e28da98c357c9c6
Guilherme Borlot Oliveira <guiborlot@edu.unifil.br>
2022-01-19 10:58:18 -0300

    Aula 05 - Módulos

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CursosModule } from './cursos/cursos.module';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 
 @NgModule({
@@ -12,7 +13,8 @@ import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
