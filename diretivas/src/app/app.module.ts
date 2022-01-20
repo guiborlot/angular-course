094cf77d6d961c489bf53e2a3fbbcdd2c906bb64
Guilherme Borlot Oliveira <guiborlot@edu.unifil.br>
2022-01-20 10:22:47 -0300

    Aula 19 - Usando pré-processadores (Sass, Less, Stylus)

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+import { TesteComponent } from './teste/teste.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DiretivaNgifComponent
+    DiretivaNgifComponent,
+    TesteComponent
   ],
   imports: [
     BrowserModule,
