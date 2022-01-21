0f6c7c3387f1841303d84c990a2c0e387c5f2e76
Guilherme Borlot Oliveira <guiborlot@edu.unifil.br>
2022-01-21 12:01:37 -0300

    Aula 44 - Criando um Pipe

--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExemploPipesComponent } from './exemplo-pipes/exemplo-pipes.component';
+import { CamelCasePipe } from './camel-case.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExemploPipesComponent
+    ExemploPipesComponent,
+    CamelCasePipe
   ],
   imports: [
     BrowserModule,
