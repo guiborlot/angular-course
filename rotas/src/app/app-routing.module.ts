71b7712bac32de516966e2fc84b8a48df2d9824d
Guilherme Borlot Oliveira <guiborlot@edu.unifil.br>
2022-01-21 15:07:47 -0300

    Aula 50 - Rotas: Configurando rotas simples

--- rotas/src/app/app-routing.module.ts
+++ rotas/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CursosComponent } from './cursos/cursos.component';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: 'cursos', component: CursosComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'home', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
