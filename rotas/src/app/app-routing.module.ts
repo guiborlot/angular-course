1e9e2bcf479c4b9f347a667b2712433b25327027
Guilherme Borlot Oliveira <guiborlot@edu.unifil.br>
2022-01-24 08:38:30 -0300

    Aula 53 - Rotas: Definindo e extraindo parâmetros de roteamento

--- rotas/src/app/app-routing.module.ts
+++ rotas/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursosComponent } from './cursos/cursos.component';
 
 import { HomeComponent } from './home/home.component';
@@ -7,6 +8,7 @@ import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'cursos', component: CursosComponent},
+  { path: 'curso/:id', component: CursoDetalheComponent},
   { path: 'login', component: LoginComponent},
   { path: '', component: HomeComponent}
 ];
