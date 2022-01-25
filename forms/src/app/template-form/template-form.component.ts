601cbe4e5f8408e2dbd44052c923d472e511e273
Guilherme Borlot Oliveira <guiborlot@edu.unifil.br>
2022-01-25 11:33:15 -0300

    Aula 74 - Forms (template drive) Controles ngForm, ngSubmit e ngModel

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Form } from '@angular/forms';
 
 @Component({
   selector: 'app-template-form',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./template-form.component.scss']
 })
 export class TemplateFormComponent implements OnInit {
+
+  onSubmit(form: Form){
+    console.log(form);
+  }
 
   constructor() { }
 
