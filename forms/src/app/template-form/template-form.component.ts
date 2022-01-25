fd995d8255860453e2c6c4208b852a7002a23c7e
Guilherme Borlot Oliveira <guiborlot@edu.unifil.br>
2022-01-25 13:24:51 -0300

    Aula 78 - Forms (template driven) Aplicando CSS na validação dos campos

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class TemplateFormComponent implements OnInit {
 
   usuario: any = {
-    nome: 'Guilherme',
-    email: 'guilherme@email.com'
+    nome: null,
+    email: null
   }
 
   onSubmit(form: any){
