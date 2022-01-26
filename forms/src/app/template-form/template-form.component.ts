73ceedb042e64e2622525b834758abb0080384df
Guilherme Borlot Oliveira <guiborlot@edu.unifil.br>
2022-01-26 10:38:13 -0300

    Aula 86 - Forms (template drive) Populando campos com setValue e patchValue (CEP)

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-template-form',
@@ -21,7 +20,7 @@ export class TemplateFormComponent implements OnInit {
     estado: null
   }
 
-  onSubmit(form: any){
+  onSubmit(form: any) {
     console.log(form.value);
 
     console.log(this.usuario);
@@ -32,30 +31,72 @@ export class TemplateFormComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  verificaValidTouched(campo: any){
+  verificaValidTouched(campo: any) {
     return !campo.valid && campo.touched;
   }
 
-  aplicaInvalidErro(campo: any){
+  aplicaInvalidErro(campo: any) {
     return {
       'is-invalid': this.verificaValidTouched(campo)
     };
   }
 
-  aplicaFeedbackErro(campo: any){
+  aplicaFeedbackErro(campo: any) {
     return {
       'invalid-feedback': this.verificaValidTouched(campo)
     };
   }
 
-  consultaCEP(cep: any){
+  consultaCEP(cep: any, form: any) {
     cep = cep.replace(/\D/g, '');
-    if(cep != ""){
+    if (cep != "") {
       let validacep = /^[0-9]{8}$/;
-      if(validacep.test(cep)){
-        this.http.get(`//viacep.com.br/ws/${cep}/json`).subscribe(res => console.log(res));
+      if (validacep.test(cep)) {
+
+        this.resetaDadosForm(form);
+
+        this.http.get(`//viacep.com.br/ws/${cep}/json`).subscribe(res => this.populaDadosForm(res, form));
       }
     }
+  }
+
+  populaDadosForm(dados: any, formulario: any) {
+    // formulario.setValue({
+    //   nome: formulario.value.nome,
+    //   email: formulario.value.email,
+    //   endereco: {
+    //     cep: dados.cep,
+    //     numero: '',
+    //     complemento: dados.complemento,
+    //     rua: dados.logradouro,
+    //     bairro: dados.bairro,
+    //     cidade: dados.localidade,
+    //     estado: dados.uf
+    //   }
+    // });
+
+    formulario.form.patchValue({
+      endereco: {
+        cep: dados.cep,
+        complemento: dados.complemento,
+        rua: dados.logradouro,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });
+  }
+
+  resetaDadosForm(formulario: any){
+    formulario.form.patchValue({
+      endereco: {
+        complemento: null,
+        rua: null,
+        bairro: null,
+        cidade: null,
+        estado: null
+      }
+    })
   }
 
 
