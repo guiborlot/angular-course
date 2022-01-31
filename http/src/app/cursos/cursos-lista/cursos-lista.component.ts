4bc15d5db4d0e7b181bdde05b00c04a70c281246
Guilherme Borlot Oliveira <guiborlot@edu.unifil.br>
2022-01-31 14:51:23 -0300

    Aula 121 - Http: Dica: Variável de Ambiente

--- http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { CursosService } from '../cursos.service';
+import { Curso } from '../curso';
+import { Observable, empty, of, Subject, EMPTY } from 'rxjs';
+import { catchError, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cursos-lista',
   templateUrl: './cursos-lista.component.html',
-  styleUrls: ['./cursos-lista.component.scss']
+  styleUrls: ['./cursos-lista.component.scss'],
+  preserveWhitespaces: true
 })
 export class CursosListaComponent implements OnInit {
 
-  constructor() { }
+  //cursos!: Curso[];
+  cursos$!: Observable<Curso[]>;
+  error$ = new Subject<boolean>();
 
-  ngOnInit(): void {
+  constructor(private service: CursosService) { }
+
+  ngOnInit() {
+    // this.service.list()
+    //   .subscribe(dados => this.cursos = dados);
+
+    this.onRefresh();
+  }
+
+  onRefresh() {
+     this.cursos$ = this.service.list()
+     .pipe(
+       // map(),
+       // tap(),
+       // switchMap(),
+       catchError(error => {
+         console.error(error);
+         this.error$.next(true);
+         return EMPTY;
+       })
+     );
+
+  //    this.service.list()
+  //    .pipe(
+  //      catchError(error => empty())
+  //    )
+  //    .subscribe(
+  //      dados => {
+  //        console.log(dados);
+  //      }
+  //      // ,error => console.error(error),
+  //     // () => console.log('Obserservable completo!')
+  //    );
+  // 
   }
 
 }
