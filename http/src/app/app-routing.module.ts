a247a07c3193458e060f3ed328dbeaef418c93ef
Guilherme Borlot Oliveira <guiborlot@edu.unifil.br>
2022-02-02 14:13:02 -0300

    Aula 135 - Upload de Arquivo: Formulário Upload com Bootstrap 5

--- http/src/app/app-routing.module.ts
+++ http/src/app/app-routing.module.ts
@@ -1,20 +1,31 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'cursos' },
-  {
-    path: 'cursos',
-    loadChildren: () => import('./cursos/cursos.module').then(m => m.CursosModule)
-  },
-  {
-    path: 'rxjs-poc',
-    loadChildren: () => import('./unsubscribe-rxjs/unsubscribe-rxjs.module').then(m => m.UnsubscribeRxjsModule)
-  }
+    { path: "", pathMatch: "full", redirectTo: "upload" },
+    {
+        path: "cursos",
+        loadChildren: () =>
+            import("./cursos/cursos.module").then((m) => m.CursosModule),
+    },
+    {
+        path: "upload",
+        loadChildren: () =>
+            import("./upload-file/upload-file.module").then(
+                (m) => m.UploadFileModule
+            ),
+    },
+    {
+        path: "rxjs-poc",
+        loadChildren: () =>
+            import("./unsubscribe-rxjs/unsubscribe-rxjs.module").then(
+                (m) => m.UnsubscribeRxjsModule
+            ),
+    },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
