d6bd9972e7aa778f7cda17d2ab6ac190d18f46d6
Guilherme Borlot Oliveira <guiborlot@edu.unifil.br>
2022-02-03 11:36:50 -0300

    Aula 143 - Http: Passando Parâmetros na URL (HttpParams)

--- http/src/app/reactive-search/lib-search/lib-search.component.ts
+++ http/src/app/reactive-search/lib-search/lib-search.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
 import { FormControl } from "@angular/forms";
+import { map, Observable, tap } from "rxjs";
 
 @Component({
     selector: "app-lib-search",
@@ -8,12 +10,38 @@ import { FormControl } from "@angular/forms";
 })
 export class LibSearchComponent implements OnInit {
     queryField = new FormControl();
+    readonly SEARCH_URL = "https://api.cdnjs.com/libraries";
+    results$!: Observable<any>;
+    total!: number;
 
-    constructor() {}
+    constructor(private http: HttpClient) {}
 
     ngOnInit(): void {}
 
     onSearch() {
+        const fields = 'name,description,version,homepage'
         console.log(this.queryField.value);
+
+        let value = this.queryField.value;
+        if (value && (value = value.trim()) !== "") {
+
+            const params_ = {
+                search: value,
+                fields: fields
+            }
+
+            let params = new HttpParams()
+            params = params.set('search', value);
+            params = params.set('fields', fields)
+
+            this.results$ = this.http
+                .get(
+                    this.SEARCH_URL, { params }
+                )
+                .pipe(
+                    tap((res: any) => (this.total = res.total)),
+                    map((res: any) => res.results)
+                );
+        }
     }
 }
