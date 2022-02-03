53863f2a02e5d688207df3a460784155c742ddf5
Guilherme Borlot Oliveira <guiborlot@edu.unifil.br>
2022-02-03 08:41:34 -0300

    Aula 137 - Http Upload de Arquivo: Request com FormData

--- http/src/app/upload-file/upload-file/upload-file.component.ts
+++ http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UploadFileService } from "../upload-file.service";
 
 @Component({
-  selector: 'app-upload-file',
-  templateUrl: './upload-file.component.html',
-  styleUrls: ['./upload-file.component.scss']
+    selector: "app-upload-file",
+    templateUrl: "./upload-file.component.html",
+    styleUrls: ["./upload-file.component.scss"],
 })
 export class UploadFileComponent implements OnInit {
+    files!: Set<File>;
 
-  constructor() { }
+    constructor(private service: UploadFileService) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
-  onChange(event: any){
-      console.log(event);
-  }
+    onChange(event: any) {
+        console.log(event);
 
+        const selectedFiles = <FileList>event.srcElement.files;
+        this.files = new Set();
+        for(let i = 0; i < selectedFiles.length; i++){
+            this.files.add(selectedFiles[i])
+        }
+    }
+
+    onUpload() {
+        if (this.files && this.files.size > 0){
+            this.service.upload(this.files, 'http://localhost:8000/upload').subscribe(response => console.log('upload concluido'));
+        }
+    }
 }
