223534c1e8efec5c23f328b0c00dea368952edce
Guilherme Borlot Oliveira <guiborlot@edu.unifil.br>
2022-02-03 09:08:59 -0300

    Aula 138 - Http: Removendo CORS com Angular Proxy

--- http/src/app/upload-file/upload-file/upload-file.component.ts
+++ http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { environment } from "src/environments/environment";
 import { UploadFileService } from "../upload-file.service";
 
 @Component({
@@ -25,7 +26,7 @@ export class UploadFileComponent implements OnInit {
 
     onUpload() {
         if (this.files && this.files.size > 0){
-            this.service.upload(this.files, 'http://localhost:8000/upload').subscribe(response => console.log('upload concluido'));
+            this.service.upload(this.files, environment.BASE_URL + '/upload').subscribe(response => console.log('upload concluido'));
         }
     }
 }
